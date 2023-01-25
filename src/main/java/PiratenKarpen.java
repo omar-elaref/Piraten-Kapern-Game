3b5c43ea0f15ebdb30fb87ce4f130f876ead1ab7
Omar El Aref <o.k.elaref@gmail.com>
2023-01-24 20:43:08 -0500

    almost done step 4

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -11,9 +11,9 @@ public class PiratenKarpen {
     public static void main(String[] args) {
         System.out.println("Welcome to Piraten Karpen Simulator!");
         System.out.println("I'm rolling a dice");
-        Dice myDice = new Dice();
-        System.out.println(myDice.roll());
-        System.out.println("That's all folks!");
+        //Dice myDice = new Dice();
+        //System.out.println(myDice.roll());
+        //System.out.println("That's all folks!");
 
         Game firstTry = new Game();
 
